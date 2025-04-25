1c137e8ad79ddce9e4e6b5718846da3ff09ab2a0
mn <eaglel4rk@gmail.com>
2025-04-25 23:42:09 +0900

    url環境設定修正

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -7,7 +7,7 @@ const options = {
 
 const client = applyCaseMiddleware(
     axios.create({
-        baseURL: '${process.env.REACT_APP_BASE_URL}/api/v1',
+        baseURL: '${import.meta.env.VITE_BASE_URL}/api/v1',
     }),
     options
 );
