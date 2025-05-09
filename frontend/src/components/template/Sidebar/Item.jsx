ed89ceea85bd3752c016554acabcd5592fab5d9b
mn <eaglel4rk@gmail.com>
2025-05-10 00:04:53 +0900

    Itemコンポーネント化

---
+++ frontend/src/components/template/Sidebar/Item.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { TemplateContext } from "../../context/TemplateContext"
+
+export default function Item({ template, categoryName }) {
+  const { selectItem, selectedItem } = useContext(TemplateContext);
+  return (
+    <li
+      key={template.id}
+      className={`flex flex-col px-2 py-1 text-sm border-b border-gray-100 cursor-pointer hover:bg-gray-50 ${
+        selectedItem?.id === template.id ? 'bg-gray-100' : ''
+      }`}
+      onClick={() => selectItem(template.title)}
+    >
+      <p className="text-base">{template.title}</p>
+      <span className="text-xs text-blue-600 my-1">{categoryName}</span>
+    </li>
+  )
+}
