03b6b7164600f08924cb180a18072a3b7e9244c4
mn <eaglel4rk@gmail.com>
2025-05-13 15:24:59 +0900

    マークダウン適用のトグルボタン実装 closes #47

--- frontend/src/components/template/Content/Preview.jsx
+++ frontend/src/components/template/Content/Preview.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import Markdown from "react-markdown";
 
 export default function Preview({bodyInput}) {
+  const [ isMarkdown, setIsMarkdown ] = useState(true);
 
   return(
     <div className="flex-1 p-4">
-      <h3 className="text-lg font-medium mb-1">プレビュー</h3>
-      <p className="text-xs text-gray-500 mb-3">マークダウンが適用された状態で表示されます</p>
+      <div className="flex justify-between">
+        <div>
+          <h3 className="text-lg font-medium mb-1">プレビュー</h3>
+          <p className="text-xs text-gray-500 mb-3">マークダウンが適用された状態で表示されます</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <label htmlFor="checkbox">マークダウン適用</label>
+          <input 
+            id="checkbox"
+            type="checkbox" 
+            className="toggle toggle-sm toggle-primary "
+            defaultChecked
+            onClick={() => setIsMarkdown(!isMarkdown)}
+            />
+        </div>
+      </div>
       
       <div className="w-full h-full border border-gray-300 rounded px-5 pt-2 pb-20">
         <div className="prose prose-sm">
-          <Markdown>{bodyInput}</Markdown>
+          {isMarkdown ? <Markdown>{bodyInput}</Markdown> : bodyInput}
         </div>
       </div>
     </div>
