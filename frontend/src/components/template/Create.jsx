701cab5732370f281b0a818fd9052155bef3ebe7
mn <eaglel4rk@gmail.com>
2025-05-15 14:54:00 +0900

    create作成画面にバリデーションメッセージ実装

--- frontend/src/components/template/Create.jsx
+++ frontend/src/components/template/Create.jsx
@@ -1,20 +1,23 @@
 import Markdown from "react-markdown";
 import Header from "../Header";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { TemplateContext } from "../context/TemplateContext";
 import Button from "../Button";
+import { useForm } from "react-hook-form";
+import remarkBreaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm'
+
 
 export default function Create() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [category, setCategory] = useState("");
-  
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({ mode: "onBlur" })
   const { handleCreateTemplate } = useContext(TemplateContext);
   const navigate = useNavigate();
 
+  const bodyValue = watch("body");
 
-  const create = () => {
+  const create = (data) => {
+    const { title, body, category } = data;
     handleCreateTemplate(title, body, category);
     navigate("/template");
   }
@@ -28,11 +31,26 @@ export default function Create() {
           <h1 className="text-xl font-semibold">新規テンプレート作成</h1>
           <div className="flex flex-col my-3">
             <label htmlFor="title">テンプレート名</label>
-            <input type="text" placeholder="テンプレート名を入力" className="input" value={title} onChange={e => setTitle(e.target.value)}/>
+            <input
+              type="text"
+              placeholder="テンプレート名を入力"
+              className="input"
+              {...register("title", {
+                required: "タイトルは必須です"
+              })}
+              />
+            <p className="text-red-400">{errors?.title?.message}</p>
           </div>
           <div className="flex flex-col">
             <label htmlFor="body">テンプレート内容</label>
-            <textarea name="body" id="body" cols="30" rows="30" className="textarea w-full" value={body} onChange={(e) => setBody(e.target.value)} />
+            <textarea
+              name="body"
+              id="body"
+              cols="30"
+              rows="30"
+              className="textarea w-full"
+              {...register("body")}
+              />
           </div>
         </div>
 
@@ -40,18 +58,30 @@ export default function Create() {
           <h2 className="text-md font-semibold">プレビュー</h2>
           <div className="flex flex-col my-3 h-24">
             <label htmlFor="category">カテゴリー</label>
-            <input type="text" placeholder="カテゴリ名を入力" className="input" value={category} onChange={e => setCategory(e.target.value)} />
+            <input
+              type="text"
+              placeholder="カテゴリ名を入力"
+              className="input"
+              {...register("category", {
+                required: "カテゴリ名は必須です"
+              })}
+              />
+            <p className="text-red-400">{errors?.category?.message}</p>
+
           </div>
           <div className="w-full min-h-[70%] max-h-screen border border-gray-300 rounded px-5 pt-2 pb-20">
             <div className="prose prose-sm">
-              <Markdown>{body}</Markdown>
+              <Markdown
+                remarkPlugins={[remarkBreaks, remarkGfm]}>
+                {bodyValue}
+              </Markdown>
             </div>
           </div>
           <div className="flex justify-end gap-3 mt-5">
             <Link to="/template">
               <Button>キャンセル</Button>
             </Link>
-            <Button className="btn-primary" onClick={() => create()}>保存</Button>
+            <Button className="btn-primary" onClick={handleSubmit(create)}>保存</Button>
           </div>
         </div>
       </div>
