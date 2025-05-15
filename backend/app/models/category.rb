01a12e0ac467558379320d345947cb003e4dc8c4
mn <eaglel4rk@gmail.com>
2025-05-15 14:29:57 +0900

    templatesとcategoriesのバリデーション設定

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :templates, dependent: :destroy
   belongs_to :user
 
+  validates :name, presence: true
 end
--- backend/app/models/template.rb
+++ backend/app/models/template.rb
@@ -1,4 +1,8 @@
 class Template < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true
+  validates :category_id, presence: true
+
 end
