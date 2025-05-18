6fd0e23a7f7755f551e1de68c9fb9154c91852c9
mn <eaglel4rk@gmail.com>
2025-05-18 23:05:15 +0900

    ローディング実装

--- frontend/src/components/template/Template.jsx
+++ frontend/src/components/template/Template.jsx
@@ -3,20 +3,29 @@ import { Content, NoContent } from './Content/Content';
 import Header from '../Header';
 import { useContext } from 'react';
 import { TemplateContext } from '../context/TemplateContext';
+import Loading from '../Loading';
 
 export default function Template() {
-  const { templates } = useContext(TemplateContext);
+  const { templates, isLoading } = useContext(TemplateContext);
   const isEmpty = templates.length === 0;
   
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
+
   return (
     <div className='flex flex-col h-screen flex-1'>
       <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-        <div className="flex-1" >
-          {isEmpty ? <NoContent /> : <Content /> }
+      {isLoading ?
+        <Loading />
+        :
+        <div className="flex flex-1">
+          <Sidebar />
+          <div className="flex-1" >
+            {isEmpty ? <NoContent /> : <Content /> }
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 }
