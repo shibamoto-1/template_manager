db11e5e3ea8d9737130f743f97d674d59c9b1de9
mn <eaglel4rk@gmail.com>
2025-05-18 23:58:51 +0900

    サンプルテンプレートを追加

--- backend/app/controllers/api/v1/guest_sessions_controller.rb
+++ backend/app/controllers/api/v1/guest_sessions_controller.rb
@@ -5,11 +5,23 @@ class Api::V1::GuestSessionsController < ApplicationController
       password: SecureRandom.hex(10),
     )
 
-    category = @resource.categories.create!(name: "demo")
-    template = @resource.templates.create!(
-      title: "demo",
+    category_1 = @resource.categories.create!(name: "サンプル")
+    category_2 = @resource.categories.create!(name: "AA")
+
+    template_1 = @resource.templates.create!(
+      title: "テンプレート1",
       body: "## サンプルです\n\n### リスト\n\n- ",
-      category: category
+      category: category_1
+    )
+    template_2 = @resource.templates.create!(
+      title: "ハート",
+      body: "   ｡ﾟﾟ･｡･ﾟﾟ。\n   ﾟ。　 　｡ﾟ\n   　ﾟ･｡･ﾟ",
+      category: category_2
+    )
+    template_3 = @resource.templates.create!(
+      title: "雨",
+      body: "、ヽ｀、ヽ｀｀｀、ヽ｀｀、ヽ｀、ヽ｀ヽ｀、、ヽ｀ヽ｀、ヽ｀、ヽ｀\n｀、ヽ｀ヽ｀、、ヽ｀ヽ｀、ヽ(ノ；Д；)ノ ｀、、ヽ｀ヽ｀、ヽ｀｀、\n｀ヽ｀、ヽ｀、ヽ｀｀、ヽ｀、ヽ｀ヽ｀、、ヽ｀ヽヽ｀、ヽ｀｀、ヽ｀\n",
+      category: category_2
     )
 
     render json: { token: @resource.create_new_auth_token}
