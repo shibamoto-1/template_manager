a5af5ebdea6d2160a9a0328e6c066d71f732fe99
mn <eaglel4rk@gmail.com>
2025-06-08 18:33:34 +0900

    Google認証恐らくできた。（React側のhttponly火ができていない）

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,10 +1,10 @@
-class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
-
+  
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, omniauth_providers: [:google_oauth2]
+  
   has_many :templates
   has_many :categories
-
 end
--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -8,6 +8,7 @@ const options = {
 const client = applyCaseMiddleware(
     axios.create({
         baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+        withCredentials: true,
     }),
     options
 );
